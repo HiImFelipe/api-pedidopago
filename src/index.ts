756f4c399e1c7c51af774d4d36d32a4b536bbe89
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-11 01:21:49 -0300

    feat: Create a routes file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.listen(8088);
+app.listen(8088, () => {
+	console.log("Server started on port 8088");
+});
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const router = new Router()
+
+// router.get("/pharmacies/:id", PharmacyController.show);
+// router.post("/pharmacies", PharmacyController.store);
+
+// router.get("/products/:id", ProductController.show);
+// router.get("/products", ProductController.index);
+
+module.exports = router;
